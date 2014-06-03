bc5f870630a277563327206ba1cb87c9edaf32b4
Lucas Pfister <lepswiss@gmail.com>
2014-06-03 01:03:55 -0700

    Initial snippet base

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-
+    url(r'^snippets/$', 'snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail')
 )
