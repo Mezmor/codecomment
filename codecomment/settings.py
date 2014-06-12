f60b9f8f30a4daa89c828e7d79ae7b9136ca102c
Lucas Pfister <lepswiss@gmail.com>
2014-06-11 17:23:00 -0700

    Removed unused app imports from rest_framework

--- codecomment/settings.py
+++ codecomment/settings.py
@@ -55,10 +55,6 @@ WSGI_APPLICATION = 'codecomment.wsgi.application'
 
 # REST Framework
 REST_FRAMEWORK = {
-    # Use hyperlinked styles by default.
-    # Only used if the `serializer_class` attribute is not set on a view.
-    'DEFAULT_MODEL_SERIALIZER_CLASS':
-        'rest_framework.serializers.HyperlinkedModelSerializer',
 }
 
 
