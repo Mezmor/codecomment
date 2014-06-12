09cb9ad39a361aefc817056958ab26309c442739
Lucas Pfister <lepswiss@gmail.com>
2014-06-11 17:23:27 -0700

    Added a simple test for user creation and authentication

--- reviewer/tests.py
+++ reviewer/tests.py
@@ -4,4 +4,19 @@ from rest_framework.test import APITestCase
 
 
 class AccountTests(APITestCase):
-    pass
+    def test_user_creation_login(self):
+        """
+        Verify we can create a new user
+        Verify we can login with the newly created user
+        """
+        url = reverse('user-list-create')
+        post_data = {'username': 'UserA', 'password' : '123456'}
+        expected_result = {'id': 1, 'username': 'UserA', 'snippets': []}
+        response = self.client.post(url, post_data, format='json')
+
+        # Verify creation
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, expected_result)
+        # Verify Login
+        self.assertTrue(self.client.login(username='UserA', password='123456'))
+
